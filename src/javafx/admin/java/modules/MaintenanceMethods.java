aa970f0ebbe9ebe05a2ef525e692f0b9893c33cc
Jan Leppich <jan_leppich@web.de>
2018-06-14 11:14:48 +0200

    started MethodCollection for Maintenance

---
+++ src/javafx/admin/java/modules/MaintenanceMethods.java
@@ -0,0 +1,19 @@
+package javafx.admin.java.modules;
+
+import pmt.spielspaß.codegenerierung.QRCode;
+
+public class MaintenanceMethods {
+
+    public MaintenanceMethods(){}
+
+    public void deleteQRCode(QRCode qrCode){
+        /*
+         * Vorgänger des QRCodes kriegt dessen Nachfolger als Nachfolger reingeschrieben
+         */
+    }
+
+    public void updateQRCode(QRCode oldQRCode, QRCode updatedQRCode){
+
+    }
+
+}
